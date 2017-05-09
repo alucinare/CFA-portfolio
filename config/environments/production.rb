c3fc09c0eaf8cc7c0d5490e77e28c12153908d33
John S <alucinare@gmail.com>
2017-05-09 21:53:38 +1000

    MailG config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              ENV["MAILGUN_SMTP_SERVER"],
-    port:                 ENV["MAILGUN_SMTP_PORT"],
     user_name:            ENV["MAILGUN_SMTP_LOGIN"],
     password:             ENV["MAILGUN_SMTP_PASSWORD"],
     domain:               "johnstewartportfolio.herokuapp.com/",
